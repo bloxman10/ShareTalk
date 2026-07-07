54e9f9063742cf447ee51fa28924e90b6ecb58aa
bloxman10 <bloxman10009@gmail.com>
2026-07-07 17:42:29 +0300

    fix api url

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://share-talk-one.vercel.app/"
+    "https://share-talk-one.vercel.app"
   ],
   credentials: true
 }));
